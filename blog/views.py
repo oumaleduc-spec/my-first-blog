360046a85af985c30e3a03485dd18e7c577a14d3
oumax <oumaleduc@gmail.com>
2026-04-28 09:50:25 +0200

    Set InsultHub as homepage

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,7 @@ from .models import Post
 from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/insulthub.html')
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
